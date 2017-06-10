691d8abd95a332375bfa413acb4a5adce6a17cf3
William Homer <William Homer>
2017-06-10 13:15:38 -0500

    master: updating js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,31 @@
+
+// store
+// search term
+var searchTerm = 'basketball';
+// seach records to Retrueve
+var searchRecords = 10;
+// Seach StartYear
+var searchStartYear = '20170610'; //YYYYMMDD
+// Search EndYear
+var searchEndYear = '20170610'; //YYYYMMDD
+
+// Make API Call, pass in paramenters
+
+
+
+// Create Article Div
+
+// Add Article Content
+
+
+
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-//  'api-key': ,
+url += '?9669e295a3e64ec6bcda97a5e33b6b39' + $.param({
+	'api-key': "9669e295a3e64ec6bcda97a5e33b6b39",
+  'q': searchTerm,
+  'begin_date': searchStartYear,
+  'end_date': searchEndYear,
+  'sort': "newest"
 });
 
 $.ajax({
